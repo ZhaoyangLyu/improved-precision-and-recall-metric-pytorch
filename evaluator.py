ec75b1461476c8ec9f2243b41a72f8bfe1d7444d
Zhaoyang Lyu <43465228+ZhaoyangLyu@users.noreply.github.com>
2022-07-29 22:01:39 +0800

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -417,7 +417,6 @@ class DistanceBlock:
 
 
 def _batch_pairwise_distances(U, V):
-    pdb.set_trace()
     """
     Compute pairwise distances between two batches of feature vectors.
     """
@@ -429,8 +428,8 @@ def _batch_pairwise_distances(U, V):
         # norm_u as a column and norm_v as a row vectors.
         norm_u = tf.reshape(norm_u, [-1, 1])
         norm_v = tf.reshape(norm_v, [1, -1])
-        print('norm_u', norm_u)
-        print('norm_v', norm_v)
+        # print('norm_u', norm_u)
+        # print('norm_v', norm_v)
 
         # Pairwise squared Euclidean distances.
         D = tf.maximum(norm_u - 2 * tf.matmul(U, V, False, True) + norm_v, 0.0)
