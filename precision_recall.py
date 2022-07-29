b2d076bb1dbe8db7bc82379b7c18f4b5801988f1
Zhaoyang Lyu <43465228+ZhaoyangLyu@users.noreply.github.com>
2022-07-29 21:15:12 +0800

    fix bug

--- precision_recall.py
+++ precision_recall.py
@@ -10,6 +10,7 @@
 import numpy as np
 # import tensorflow as tf
 import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from time import time
 
 #----------------------------------------------------------------------------
