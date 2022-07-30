ad835b549321e8ce84348c55620793bd70ccb33c
Zhaoyang Lyu <43465228+ZhaoyangLyu@users.noreply.github.com>
2022-07-30 11:32:01 +0800

    Update save_cifar10_images.py

--- save_cifar10_images.py
+++ save_cifar10_images.py
@@ -1,10 +1,16 @@
 import torch
 import torchvision
+import os
 import pdb
+from tqdm import tqdm
 
 if __name__ == '__main__':
     dataset = torchvision.datasets.CIFAR10('./', train=True, transform=None, target_transform = None, download = True)
-
-    for i in range(len(dataset)):
-        x = dataset.__getitem__(i)
+    save_path = './datasets/cifar10'
+    os.makedirs(save_path, exist_ok=True)
+    for i in tqdm(range(len(dataset))):
+        x, label = dataset.__getitem__(i)
+        save_name = '%d_label_%d.png' % (i, label)
+        save_name = os.path.join(save_path, save_name)
+        x.save(save_name)
         pdb.set_trace()
